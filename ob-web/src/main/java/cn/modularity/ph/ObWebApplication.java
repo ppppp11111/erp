1fdb284ebd6aaf892c503f9fc5b3963395d4d11a
ph <@jingji_2>
2020-04-08 16:15:31 +0800

    第一次提交

---
+++ ob-web/src/main/java/cn/modularity/ph/ObWebApplication.java
@@ -0,0 +1,13 @@
+package cn.modularity.ph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ObWebApplication.class, args);
+    }
+
+}
